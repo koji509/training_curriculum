66c4bec8323359a9031742cf15445ead9cf72ece
koji509 <thislifek@yahoo.co.jp>
2020-11-20 20:17:47 +0900

    Issue2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    Params.require(:calendars).permit(:date, :plan)
   end
 
   def getWeek
@@ -31,8 +31,8 @@ class CalendarsController < ApplicationController
 
     7.times do |x|
       today_plans = []
-      plan = plans.map do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
+        plan = Plans.map do |plan|
+          Today_plans.push(plan.plan) if Plan.date == @todays_date + x
       end
       days = { month: '(@todays_date + x).month', date: '(@todays_date+x).day', plans: 'today_plans'}
       @week_days.push(days)
